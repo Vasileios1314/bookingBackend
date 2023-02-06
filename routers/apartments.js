187dcddb850626703e39c1a5c3edfd3c17383495
Vasileios1314 <vasilis.bobonis@hotmail.com>
2023-02-06 21:13:08 +0100

    add apartments router incl the rest

--- routers/apartments.js
+++ routers/apartments.js
@@ -1,12 +1,17 @@
 const { Router } = require("express");
 const auth = require("../auth/middleware");
 const Apartments = require("../models").apartment;
+const Comments = require("../models").comment;
+const Ratings = require("../models").rating;
+const Availability = require("../models").availability;
 
 const router = new Router();
 
 router.get("/", async (req, res) => {
   try {
-    const apartments = await Apartments.findAll();
+    const apartments = await Apartments.findAll({
+      include: [Comments, Ratings, Availability],
+    });
     res.send(apartments);
   } catch (e) {
     console.log(e.message);
