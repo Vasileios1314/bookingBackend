0d7b39e02c864f2cceeac88f41c97de46a5ff908
Vasileios1314 <vasilis.bobonis@hotmail.com>
2023-02-12 17:12:25 +0100

    get apartmentById

--- routers/apartments.js
+++ routers/apartments.js
@@ -18,4 +18,20 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res, next) => {
+  try {
+    const apartmentId = parseInt(req.params.id);
+    const apartment = await Apartments.findByPk(apartmentId, {
+      include: [Comments, Ratings, Availability],
+    });
+    if (apartment) {
+      res.send(apartment);
+    } else {
+      res.status(404).send("Apartment not found");
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
 module.exports = router;
