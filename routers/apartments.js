2a4f4f5f506ce6e8f1e7f58cbd2f7a189f8f1ea7
Vasileios1314 <vasilis.bobonis@hotmail.com>
2023-05-29 19:18:14 +0200

    add /newRating and /newComment

--- routers/apartments.js
+++ routers/apartments.js
@@ -47,4 +47,72 @@ router.get("/:id", async (req, res, next) => {
   }
 });
 
+router.post("/newRating", auth, async (req, res, next) => {
+  try {
+    const { apartmentId, rating, userId } = req.body;
+
+    // Check if the user has already rated the apartment
+    const existingRating = await Ratings.findOne({
+      where: {
+        apartmentId: apartmentId,
+        userId: userId,
+      },
+    });
+
+    if (existingRating) {
+      // User has already rated the apartment
+      return res.status(400).send("You have already rated this apartment.");
+    }
+
+    // Create a new rating
+    const newRating = await Ratings.create({
+      apartmentId,
+      rating,
+      userId,
+    });
+
+    if (newRating) {
+      res.send(newRating);
+    } else {
+      res.status(404).send("newRating not found");
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.post("/newComment", auth, async (req, res, next) => {
+  try {
+    const { apartmentId, comment, userId } = req.body;
+
+    // Check if the user has already comment the apartment
+    const existingComment = await Comments.findOne({
+      where: {
+        apartmentId: apartmentId,
+        userId: userId,
+      },
+    });
+
+    if (existingComment) {
+      // User has already comment the apartment
+      return res.status(400).send("You have already reviewed this apartment.");
+    }
+
+    // Create a new comment
+    const newComment = await Comments.create({
+      apartmentId,
+      comment,
+      userId,
+    });
+
+    if (newComment) {
+      res.send(newComment);
+    } else {
+      res.status(404).send("newComment not found");
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
 module.exports = router;
